b8bd508e33cdc7e33eaff81a8206b0815862df87
TehShadow <74325660+TehShadow@users.noreply.github.com>
2022-03-05 14:57:51 +0200

    start

---
+++ Bot.py
@@ -0,0 +1,17 @@
+import os
+from discord.ext import commands
+from dotenv import load_dotenv,find_dotenv
+
+load_dotenv(find_dotenv())
+token = os.getenv("TOKEN")
+
+
+
+bot = commands.Bot(command_prefix="!")
+
+for filename in os.listdir("./commands"):
+    if filename.endswith(".py") and filename !="__init__.py":
+        bot.load_extension(f'cogs.{filename[:-3]}')
+
+print("I am online!")
+bot.run(token)
