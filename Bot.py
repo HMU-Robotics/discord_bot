35bf5efac33a847483af1be33bba90059fb5b8c4
TehShadow <74325660+TehShadow@users.noreply.github.com>
2022-03-10 00:05:52 +0200

    Base

--- Bot.py
+++ Bot.py
@@ -1,17 +1,34 @@
 import os
-from discord.ext import commands
+from sys import intern
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
 from dotenv import load_dotenv,find_dotenv
+
 
 load_dotenv(find_dotenv())
 token = os.getenv("TOKEN")
 
 
+interns = nextcord.Intents.default()
+interns.members = True
+bot = commands.Bot(command_prefix="!",interns=interns)
 
-bot = commands.Bot(command_prefix="!")
+@bot.event
+async def on_ready():
+    print(f'{bot.user} has logged in.')
+    
+#loads cogs
+initial_extensions = []
 
-for filename in os.listdir("./commands"):
-    if filename.endswith(".py") and filename !="__init__.py":
-        bot.load_extension(f'commands.{filename[:-3]}')
+for filename in os.listdir("./cogs"):
+        if filename.endswith(".py") and filename !="__init__.py":
+            initial_extensions.append(f'cogs.{filename[:-3]}')
+
+if __name__ == '__main__':
+    for extension in initial_extensions:
+        bot.load_extension(extension)
+############################################################################
 
 print("I am online!")
 bot.run(token)
---
+++ cogs/test.py
@@ -0,0 +1,22 @@
+
+
+import nextcord
+from nextcord import Interaction
+from nextcord.ext import commands
+
+guildID = 806373333560983562
+
+
+class Test(commands.Cog):
+    def __init__(self,client):
+        self.client = client
+        
+
+    @nextcord.slash_command(name='hello',description="Test",guild_ids=[guildID])
+    async def test(self,interaction:Interaction):
+        await interaction.response.send_message("Test")
+    
+
+
+def setup(bot):
+    bot.add_cog(Test(bot))
